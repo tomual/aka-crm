ccba87acfc48fdac640b938bff74da630fc23d6c
tomual <tomual@users.noreply.github.com>
2018-12-04 22:33:23 +0800

    Read from database

--- aka-crm/MainWindow.xaml.cs
+++ aka-crm/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +25,48 @@ namespace aka_crm
         public MainWindow()
         {
             InitializeComponent();
+
+            try
+            {
+                // Build connection string
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+                builder.DataSource = "DESKTOP-HBKMI7B\\SQLEXPRESS";
+                builder.IntegratedSecurity = true;
+                builder.InitialCatalog = "master";
+
+                // Connect to SQL
+                Console.Write("Connecting to SQL Server ... ");
+                using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+                {
+                    Console.WriteLine("Done.");
+                    SqlCommand sql = new SqlCommand();
+                    sql.CommandText = "SELECT * FROM Testing";
+                    sql.CommandType = System.Data.CommandType.Text;
+                    sql.Connection = connection;
+                    connection.Open();
+                    SqlDataReader reader = sql.ExecuteReader();
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            Console.WriteLine("{0}\t{1}", reader.GetInt32(0),
+                                reader.GetString(1));
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("No rows found.");
+                    }
+
+
+                }
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            Console.WriteLine("All done. Press any key to finish...");
         }
     }
 }
