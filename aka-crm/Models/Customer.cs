7c02f2a3d4663ca8a6dfe0f07f2fcd8edb988c10
tomual <tomual@users.noreply.github.com>
2018-12-09 21:06:13 +0800

    Customer model

---
+++ aka-crm/Models/Customer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aka_crm.Models
+{
+    class Customer : INotifyPropertyChanged
+    {
+        public Customer(string customerName)
+        {
+            Name = customerName;
+        }
+
+        private string _Name;
+        public string Name
+        {
+            get
+            {
+                return _Name;
+            }
+            set
+            {
+                _Name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        #endregion
+    }
+}
