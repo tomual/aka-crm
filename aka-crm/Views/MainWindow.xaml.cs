4e8b7690e62dbe39c367fffaa121cb7ea74364c2
tomual <tomual@users.noreply.github.com>
2018-12-10 20:44:12 +0800

    TableAdapter is a mistake

--- aka-crm/Views/MainWindow.xaml.cs
+++ aka-crm/Views/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using aka_crm.Models;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Diagnostics;
 using System.Linq;
@@ -25,13 +26,55 @@ namespace aka_crm
     {
         public MainWindow()
         {
+            Console.WriteLine("Hello");
             InitializeComponent();
 
-            akacrmDataSetTableAdapters.CustomerTableAdapter customerTableAdapter;
-            customerTableAdapter = new akacrmDataSetTableAdapters.CustomerTableAdapter();
+            try
+            {
+                // Build connection string
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+                builder.DataSource = "DESKTOP-HBKMI7B\\SQLEXPRESS";
+                builder.IntegratedSecurity = true;
+                builder.InitialCatalog = "aka-crm";
 
-            // Insert
-            customerTableAdapter.Insert("Steve's Shoe Shop", DateTime.Now, DateTime.Now);
+                // Connect to SQL
+                using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+                {
+                    SqlCommand sql = new SqlCommand();
+                    connection.Open();
+
+                    String query = "INSERT INTO Company (name, created, modified) VALUES (@name, @created, @modified)";
+                    sql = new SqlCommand(query, connection);
+                    sql.Parameters.AddWithValue("@name", "Steve's Shoe Shop");
+                    sql.Parameters.AddWithValue("@created", DateTime.Now);
+                    sql.Parameters.AddWithValue("@modified", DateTime.Now);
+                    sql.ExecuteNonQuery();
+
+                    sql.Connection = connection;
+                    sql.CommandText = "SELECT * FROM Company";
+                    sql.CommandType = System.Data.CommandType.Text;
+                    SqlDataReader reader = sql.ExecuteReader();
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            Console.WriteLine("{0}\t{1}", reader.GetInt32(0),
+                                reader.GetString(1));
+                            CompanyName.Text = reader.GetString(1);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("No rows found.");
+                    }
+
+
+                }
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
 
         }
     }
