758e55bcd87b0fffab18821d0a44ab4556efb847
tomual <tomual@users.noreply.github.com>
2018-12-16 17:12:13 +0800

    CustomerProfile model

---
+++ aka-crm/Models/CustomerProfile.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aka_crm.Models
+{
+    class CustomerProfile : INotifyPropertyChanged
+    {
+        public int Id { get; set; }
+        public int CustomerId { get; set; }
+        public string ResponsibleParty { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+
+        public CustomerProfile()
+        {
+        }
+        
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        #endregion
+    }
+}
